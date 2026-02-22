3759e0ce8e771ec7462417a4f4fa571f1b383e4b
Oscar Castro <ibanezoscar157@gmail.com>
2026-02-21 22:54:08 -0500

    ajustar layout para carrito fijo fuera del grid y ocupar espacio correctamente

--- src/components.tsx/dashboard-store.tsx
+++ src/components.tsx/dashboard-store.tsx
@@ -21,16 +21,12 @@ export function DashboardStore(){
     return(
         <div className="bg-[#fdf6ed] w-full min-h-screen overflow-x-hidden">
             <Siderbar showMenu = {showMenu} onToggleMenu={onToggleMenu} onToggleShowCart={onToggleShowCart} showCart={showCart}/>
-            <main className="
-                grid grid-cols-1 lg:grid-cols-8
-                transition-all duration-300
-                lg:pl-28
-                ">
+            <main className="grid grid-cols-1 lg:grid-cols-6 transition-all duration-300 lg:pl-28 lg:pr-[320px]">
                 <div className="lg:col-span-6">
                     <Menu/>
                 </div>
 
-                <div className={`lg:col-span-2 fixed lg:static top-0 bottom-[64px] lg:bottom-auto right-0 w-full lg:w-auto h-auto lg:h-screen bg-[#ffffff] z-[50] transition-transform ${showCart ? "translate-x-0" : "translate-x-full"}`}>
+                <div className={`lg:col-span-fixed lg:fixed top-0 right-0 bottom-[64px] lg:bottom-0 w-full lg:w-[320px] h-full lg:h-screen bg-[#ffffff] z-[50] overflow-hidden transition-transform duration-500 ease-in-out ${showCart ? "translate-x-0" : "translate-x-full lg:translate-x-0"}`}>
                     <ShoppingCart/>
                 </div>
             </main>
